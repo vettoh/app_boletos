1af9873acff9cb98760268a9815b6ab7455caeb9
Vetoh <weverton@htamoveis.com.br>
2025-04-14 16:56:20 -0300

    seguimos com o progresso de construção da interface

--- gui.py
+++ gui.py
@@ -1,9 +1,46 @@
-from tkinter import *
+
 from tkinter import ttk
 import tkinter as Tk
 
+def confirmacao():
+    valor_nome = nome.get()
+    valor_data_de_pedido = data_compra.get() 
+    valor_da_compra = valor.get()
+    valor_parcelas = parcelas.get()
+    valor_vencimento = vencimento.get()
+
+    FRM.destroy()
+    # como destruimos tudo o que havia no primeiro FRM tivemos que cirar um novo, para a exibição da nova página
+    FRM2 = ttk.Frame(janela, padding=10)
+    FRM2.grid()
+    
+    LABEL_TITULO = Tk.Label(FRM2,text="CONFIRME SE OS DADOS ESTÃO CERTOS")
+    LABEL_TITULO.grid(column=4,row=1)
+
+    nome_FRM2 = Tk.Label(FRM2, text= f'{valor_nome}')
+    nome_FRM2.grid(column=1, row=6)
+
+    data_de_pedido_FRM2 = Tk.Label(FRM2, text= f'{valor_data_de_pedido}')
+    data_de_pedido_FRM2.grid(column=2, row=6)
+
+    valor_da_compra_FRM2 = Tk.Label(FRM2, text= f'{valor_da_compra}')
+    valor_da_compra_FRM2.grid(column=3, row=6)
+
+    parcelas_FRM2 = Tk.Label(FRM2, text= f'{valor_parcelas}')
+    parcelas_FRM2.grid(column=4, row=6)
+
+    vencimento_FRM2 = Tk.Label(FRM2, text= f'{valor_vencimento}')
+    vencimento_FRM2.grid(column=5, row=6)
+
+    botao_confirmar = Tk.Button(FRM2, text="CONFIRMAR")
+    botao_confirmar.grid(column=4, row=8)    
+    
+
+
+
+
 janela = Tk.Tk()
-largura = 800
+largura = 650
 altura = 400
 janela.geometry(f"{largura}x{altura}")
 
@@ -11,12 +48,23 @@ FRM = ttk.Frame(janela, padding=10)
 FRM.grid()
 
 #labels tabela
-TITULO = Tk.Label(FRM, text="ALERTA DE BOLETOS").grid(column=4, row=1)
-NOME = Tk.Label(FRM, text="NOME").grid(column=1, row=3)
-DATA_COMPRA = Tk.Label(FRM, text="DATA DA COMPRA").grid(column=3, row=3)
-VALOR = Tk.Label(FRM, text="VALOR").grid(column=4, row=3)
-PARCELAS = Tk.Label(FRM, text="PARCELAS").grid(column=6, row=3)
-DATA_VENCIMENTO = Tk.Label(FRM, text="VENCIMENTO").grid(column=5, row=3)
+TITULO = Tk.Label(FRM, text="ALERTA DE BOLETOS")
+TITULO.grid(column=4, row=1)
+
+NOME = Tk.Label(FRM, text="NOME")
+NOME.grid(column=1, row=3)
+
+DATA_COMPRA = Tk.Label(FRM, text="DATA DA COMPRA")
+DATA_COMPRA.grid(column=3, row=3)
+
+VALOR = Tk.Label(FRM, text="VALOR")
+VALOR.grid(column=4, row=3)
+
+PARCELAS = Tk.Label(FRM, text="PARCELAS")
+PARCELAS.grid(column=6, row=3)
+
+DATA_VENCIMENTO = Tk.Label(FRM, text="VENCIMENTO")
+DATA_VENCIMENTO.grid(column=5, row=3)
 
 #entrys
 nome = Tk.Entry(FRM)
@@ -33,6 +81,10 @@ parcelas.grid(column=5, row=4)
 
 vencimento = Tk.Entry(FRM)
 vencimento.grid(column=6, row=4)
+
+cadastrar = Tk.Button(FRM, text="CADASTRAR", command=confirmacao)
+cadastrar.grid(column=4, row=(5))
+
 
 janela.mainloop()
 
